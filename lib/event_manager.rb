225b2dd7db91115d6e80d9858c621cb912639022
PamBWillenz <pam@positek.net>
2025-02-11 18:26:32 -0500

    csv iterations with an api

---
+++ lib/event_manager.rb
@@ -0,0 +1,42 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4] # Handle nil, pad with leading zeros, and truncate to 5 characters
+end
+
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = File.read('secret.key').strip
+  #civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorLowerBody', 'legislatorUpperBody']
+    )
+    legislators = legislators.officials  
+    legislators_names = legislators.map(&:name)
+    legislators_names.join(', ')
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'event_attendees.csv', 
+  headers: true, 
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  puts "#{name} #{zipcode} #{legislators}"
+end
