a77544407a1dab92081f99429dbbaa31922b4a75
PamBWillenz <pam@positek.net>
2025-02-12 12:26:39 -0500

    remove commented secret key

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,7 +8,6 @@ end
 def legislators_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = File.read('secret.key').strip
-  #civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
     legislators = civic_info.representative_info_by_address(
